d1ec769bfdeaf84ffdc128b67acc93d457141b59
Mckelle Gravelle <GravelleMckelle@gmail.com>
2023-05-17 04:15:04 -0500

    Add constructor for Position that allows for a position to fed rather than coordinates.

--- src/main/java/ravioli/gravioli/menu/position/Position.java
+++ src/main/java/ravioli/gravioli/menu/position/Position.java
@@ -1,4 +1,10 @@
 package ravioli.gravioli.menu.position;
 
 public record Position(int x, int y) {
+    public Position(final int slot) {
+        this(
+            slot % 9,
+            slot / 9
+        );
+    }
 }
